abefcd12a3666db1994ab939f3d97ee7a80247e7
Goksel <gkslshn26@gmail.com>
2019-07-30 23:29:39 +0300

    added more page

---
+++ Lesson_1/webserver.py
@@ -0,0 +1,42 @@
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+class webserverHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            if self.path.endswith("/hello"):
+                self.send_response(200)
+                self.send_header('Content-type','text/html')
+                self.end_headers()
+
+                output = ""
+                output += "<html><body>Hello!</body></html>"
+                self.wfile.write(output)
+                print output
+                return
+            if self.path.endswith("/hola"):
+                self.send_response(200)
+                self.send_header('Content-type','text/html')
+                self.end_headers()
+
+                output = ""
+                output += "<html><body>Hola!</body></html>"
+                self.wfile.write(output)
+                print output
+                return
+        except IOError:
+            self.send_error(404,"File Not Found %s" % self.path)        
+
+def main():
+    try:
+    
+        port = "8080"
+        server = HTTPServer(("",int(port)),webserverHandler)
+        print 'Web server running...open localhost:8080/restaurants in your browser'
+        server.serve_forever()
+
+    except KeyboardInterrupt:
+        print "^C entered, stopping web server..."
+        server.socket.close()
+
+if __name__ == "__main__":
+    main()
